60c0d10a8b387ae72adea4851b61c82191234b37
Duff OMelia <duff@omelia.org>
2010-08-27 10:39:14 -0400

    Generated a rails app.
    Used v3.0.0_RC2 of Rails to generate it.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoreSample::Application.config.secret_token = '6d7cd256d354a59c779893a9f72027d0d73a434b3d6d4f2596e6592a233839802ecd8d41728c78a6e1f3cf579032d44d71ffe59df60cf766b1f404de8fb9f54e'
