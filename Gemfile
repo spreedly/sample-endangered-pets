513f46dcb6902cf06f13f695e3f640ac4e7c16b7
Duff OMelia <duff@omelia.org>
2010-09-08 17:41:55 -0400

    Showing some validation errors.

--- Gemfile
+++ Gemfile
@@ -6,3 +6,4 @@ gem "haml", '3.0.18'
 gem "mysql2", "0.2.3"
 gem "httparty", "0.6.1"
 gem "colored", "1.2"
+gem "hpricot", "0.8.2"
--- app/controllers/tshirts_controller.rb
+++ app/controllers/tshirts_controller.rb
@@ -1,4 +1,5 @@
 require 'ostruct'
+# require 'hpricot'
 
 class TshirtsController < ApplicationController
 
@@ -12,6 +13,14 @@ class TshirtsController < ApplicationController
     result = SpreedlyCore.purchase(params[:token], "2")
     if result.code == 422
       @credit_card = new_card(result["transaction"]["payment_method"])
+      errors = ActiveModel::Errors.new(@credit_card)
+
+      doc = Hpricot(result.body)
+      doc.search("transaction>payment_method>errors>error").each do |each|
+        errors.add(each.attributes['attribute'], I18n.t(each.attributes['key']))
+      end
+
+      @credit_card.errors = errors
       return render(:action => :buy_tshirt) 
     end
 
@@ -22,7 +31,10 @@ class TshirtsController < ApplicationController
   private
     def new_card(attributes = {})
       defaults = { "first_name" => nil, "last_name" => nil, "number" => nil, "verification_value" => nil }
-      OpenStruct.new(defaults.merge(attributes))
+      card = OpenStruct.new(defaults.merge(attributes))
+      card.errors = ActiveModel::Errors.new(card) 
+      card.class.extend ActiveModel::Translation
+      card
     end
 
 end
