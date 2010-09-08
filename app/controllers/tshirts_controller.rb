af7c680afa8d994eb7a26134aaea40d8ad527b5a
Duff OMelia <duff@omelia.org>
2010-09-08 10:11:50 -0400

    Now showing card field values when processing fails.

--- app/controllers/tshirts_controller.rb
+++ app/controllers/tshirts_controller.rb
@@ -1,19 +1,28 @@
+require 'ostruct'
 
 class TshirtsController < ApplicationController
 
   before_filter :login_required
 
   def buy_tshirt
-
+    @credit_card = new_card
   end
 
   def transparent_redirect_complete
-    payment_method_token = params[:token]
-    @result = SpreedlyCore.purchase(payment_method_token, "2")
-    puts @result.code.to_s.magenta
-    puts @result.body.yellow
-    # return render(:action => :buy_tshirt) if @result.code.to_s == "422"
+    result = SpreedlyCore.purchase(params[:token], "2")
+    if result.code == 422
+      @credit_card = new_card(result["transaction"]["payment_method"])
+      return render(:action => :buy_tshirt) 
+    end
+
     redirect_to buy_tshirt_url
   end
+
+
+  private
+    def new_card(attributes = {})
+      defaults = { "first_name" => nil, "last_name" => nil, "number" => nil, "verification_value" => nil }
+      OpenStruct.new(defaults.merge(attributes))
+    end
 
 end
