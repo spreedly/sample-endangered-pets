d78a32d5a112e91e5f0e5ec6584fca581c7f7e9b
Duff OMelia <duff@omelia.org>
2010-09-08 18:25:22 -0400

    A little cleanup.

--- app/controllers/tshirts_controller.rb
+++ app/controllers/tshirts_controller.rb
@@ -1,5 +1,4 @@
 require 'ostruct'
-# require 'hpricot'
 
 class TshirtsController < ApplicationController
 
@@ -13,14 +12,7 @@ class TshirtsController < ApplicationController
     result = SpreedlyCore.purchase(params[:token], "2")
     if result.code == 422
       @credit_card = new_card(result["transaction"]["payment_method"])
-      errors = ActiveModel::Errors.new(@credit_card)
-
-      doc = Hpricot(result.body)
-      doc.search("transaction>payment_method>errors>error").each do |each|
-        errors.add(each.attributes['attribute'], I18n.t(each.attributes['key']))
-      end
-
-      @credit_card.errors = errors
+      @credit_card.errors = validation_errors_from(result.body)
       return render(:action => :buy_tshirt) 
     end
 
@@ -35,6 +27,17 @@ class TshirtsController < ApplicationController
       card.errors = ActiveModel::Errors.new(card) 
       card.class.extend ActiveModel::Translation
       card
+    end
+
+    def validation_errors_from(body)
+      errors = ActiveModel::Errors.new(@credit_card)
+
+      doc = Hpricot(body)
+      doc.search("transaction>payment_method>errors>error").each do |each|
+        errors.add(each.attributes['attribute'], I18n.t(each.attributes['key']))
+      end
+
+      errors
     end
 
 end
