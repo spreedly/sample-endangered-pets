66007f730ccfaeba977c60204dc4f8b406f16fa0
Duff OMelia <duff@omelia.org>
2012-08-07 11:22:49 -0400

    Make the asset logging less noisy in dev.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,6 @@ CoreSample::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 end
 
