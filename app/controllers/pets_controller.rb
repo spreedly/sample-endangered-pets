b01ed20e7b83919ce756ca1bb7c066b7b0488948
Duff OMelia <duff@omelia.org>
2012-10-18 16:24:34 -0400

    Start on supporting gocardless pre-auths.
    This is a work in progress.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,4 +4,41 @@ class PetsController < ApplicationController
 
   end
 
+  def transparent_redirect_complete
+    @payment_method = PaymentMethod.new(SpreedlyCore.get_payment_method(params[:token]))
+
+    response = SpreedlyCore.authorize(@payment_method, amount_to_charge, redirect_url: pets_offsite_redirect_url, callback_url: pets_offsite_callback_url)
+    d { response.code }
+    d { response.body }
+    case response.code
+    when 200
+      return redirect_to(pets_successful_purchase_url)
+    when 202
+      return redirect_to(Transaction.new(response).checkout_url)
+    else
+      set_flash_error(response)
+      render(action: :subscribe)
+    end
+  end
+
+  def offsite_redirect
+  end
+
+  def offsite_callback
+  end
+
+
+  private
+  def amount_to_charge
+    139 * 100
+  end
+
+  def set_flash_error(response)
+    if response["errors"]
+      flash.now[:error] = response["errors"]["error"]["__content__"]
+    else
+      t = Transaction.new(response)
+      flash.now[:error] =  "#{t.message} (#{t.state.humanize})"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ CoreSample::Application.routes.draw do
 
   scope(path: "pets", controller: :pets, as: :pets) do
     match :subscribe
+    match :transparent_redirect_complete
+    match :offsite_redirect
+    match :offsite_callback, format: :xml
   end
 
   root to: "home#index"
