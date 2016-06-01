854af1ddb27978a33ad311860d096abddfb4dc48
Peter O <peter@spreedly.com>
2016-06-01 17:11:31 -0400

    Resolve environment issues on first time use

--- env.rb
+++ env.rb
@@ -7,8 +7,6 @@ ENV["RAILS_ENV"] ||= "development"
 ENV["DATABASE_ADAPTER"] ||= "postgresql"
 ENV["DATABASE_NAME"] ||= "spreedly_endangered_pets"
 ENV["DATABASE_TEST_NAME"] ||= "spreedly_endangered_pets_test"
-ENV["DATABASE_USER"] ||= "root"
-ENV["DATABASE_PASSWORD"] ||= ""
 ENV["DATABASE_ENCODING"] ||= "unicode"
 
 ENV["CORE_ENVIRONMENT_KEY"] ||= "OB9909MNZj62u9U4VAz3lAAPZcp"
@@ -20,7 +18,3 @@ ENV["CORE_GATEWAY_FOR_CREDIT_CARD"] ||=  "StuXzUJ5Khe3Wes31T0M4uNnjv9"
 ENV["CORE_GATEWAY_FOR_DWOLLA"] ||=  "rRrNqN3NcFRokpYMvY6TxyPrXDg"
 ENV["CORE_GATEWAY_FOR_PAYPAL"] ||=  "9Y5byWM9nMcas972qfvKEce4eMa"
 ENV["CORE_GATEWAY_FOR_GOCARDLESS"] ||=  "LmeT3035Un1ILf8fiHliLrLr13y"
-
-
-
-
